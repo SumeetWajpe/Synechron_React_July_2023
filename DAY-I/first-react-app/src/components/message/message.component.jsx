c7fe7b17a69c026b23eb6ff6a177c0afd5c9a619
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-17 11:44:39 +0530

    ✨ JSX for image source

--- DAY-I/first-react-app/src/components/message/message.component.jsx
+++ DAY-I/first-react-app/src/components/message/message.component.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 class Message extends React.Component {
   render() {
-    return <p>Hello !</p>;
+    return (
+      <div>
+        <img src={this.props.imageUrl} alt="" height="100px" width="150px" />
+        <h2>{this.props.msg}</h2>
+        <p>From : {this.props.from}</p>
+        <p>To : {this.props.to}</p>
+      </div>
+    );
   }
 }
 
