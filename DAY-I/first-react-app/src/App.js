35c73865f7b4b03f8f0df982755aef6d42d6de9c
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-17 12:33:31 +0530

    ✨ Adding bootstrap

--- DAY-I/first-react-app/src/App.js
+++ DAY-I/first-react-app/src/App.js
@@ -2,16 +2,46 @@ import React from "react";
 import Message from "./components/message/message.component";
 
 class App extends React.Component {
+  // coming from an endpoint
+  messages = [
+    {
+      msg: "Hey",
+      from: "John",
+      to: "Joe",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/Hello_Web_Series_%28Wordmark%29_Logo.png/1200px-Hello_Web_Series_%28Wordmark%29_Logo.png",
+    },
+    {
+      msg: "Hi",
+      from: "John",
+      to: "Joe",
+      imageUrl:
+        "https://www.shutterstock.com/shutterstock/photos/1482836018/display_1500/stock-vector-hi-banner-speech-bubble-poster-and-sticker-concept-geometric-memphis-style-with-text-hi-icon-1482836018.jpg",
+    },
+    {
+      msg: "Hola",
+      from: "John",
+      to: "Joe",
+      imageUrl:
+        "https://img.freepik.com/premium-vector/hola-hi-phrase-lettering-isolated-white-colourful-text-effect-design-vector-text-inscriptions-english-modern-creative-design-has-red-orange-yellow-colors_7280-7768.jpg?w=2000",
+    },
+    {
+      msg: "Hola",
+      from: "John",
+      to: "Joe",
+      imageUrl:
+        "https://img.freepik.com/premium-vector/hola-hi-phrase-lettering-isolated-white-colourful-text-effect-design-vector-text-inscriptions-english-modern-creative-design-has-red-orange-yellow-colors_7280-7768.jpg?w=2000",
+    },
+  ];
+
   render() {
     return (
-      <div>
-        <Message
-          msg="Hey"
-          from="John"
-          to="Joe"
-          imageUrl="https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/Hello_Web_Series_%28Wordmark%29_Logo.png/1200px-Hello_Web_Series_%28Wordmark%29_Logo.png"
-        />
-        <Message
+      <div className="row">
+        {this.messages.map(m => (
+          <Message messageDetails={m} />
+        ))}
+
+        {/* <Message
           msg="Hi"
           from="Jim"
           to="Joe"
@@ -22,7 +52,7 @@ class App extends React.Component {
           from="Joe"
           to="John"
           imageUrl="https://img.freepik.com/premium-vector/hola-hi-phrase-lettering-isolated-white-colourful-text-effect-design-vector-text-inscriptions-english-modern-creative-design-has-red-orange-yellow-colors_7280-7768.jpg?w=2000"
-        />
+        /> */}
 
         {/* <Message />
         <Message />
--- DAY-I/first-react-app/src/components/message/message.component.jsx
+++ DAY-I/first-react-app/src/components/message/message.component.jsx
@@ -3,11 +3,16 @@ import React from "react";
 class Message extends React.Component {
   render() {
     return (
-      <div>
-        <img src={this.props.imageUrl} alt="" height="100px" width="150px" />
-        <h2>{this.props.msg}</h2>
-        <p>From : {this.props.from}</p>
-        <p>To : {this.props.to}</p>
+      <div className="col-md-2">
+        <img
+          src={this.props.messageDetails.imageUrl}
+          alt=""
+          height="100px"
+          width="150px"
+        />
+        <h2>{this.props.messageDetails.msg}</h2>
+        <p>From : {this.props.messageDetails.from}</p>
+        <p>To : {this.props.messageDetails.to}</p>
       </div>
     );
   }
