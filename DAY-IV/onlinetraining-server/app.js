e8335125f0e4f255a9f8f606520b284ea24423e7
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-07-20 16:01:08 +0530

    ✨ Creating server for JWT (signing token)

---
+++ DAY-IV/onlinetraining-server/app.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var jwt = require("jsonwebtoken");
+
+const SECRET_KEY = "SECRET_KEY";
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.post("/login", (req, res) => {
+  // get uname + pwd from req.body
+  // verify if the user exists ! DB
+  // sign the token (create)
+
+  let payload = { name: req.body.uname, lastLogin: "Monday 25th" };
+  jwt.sign(payload, SECRET_KEY, { expiresIn: "2 Days" }, (err, token) => {
+    if (err) console.log(err);
+    else return res.json({ token });
+  });
+});
+
+app.listen(4000, () => {
+  console.log("Server running @ 4000 !");
+});
+module.exports = app;
